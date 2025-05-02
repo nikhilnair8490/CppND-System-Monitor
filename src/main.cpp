c9399286be14e821f1e6d63b0d9c9049a5a1d863
nkhilnair8490 <nikhilnair8490@gmail.com>
2025-05-02 20:07:37 +0530

    refactor: Clean up CMakeLists.txt and main.cpp by removing commented code

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,7 @@
-#include <iostream>
-#include <string>
-
-#include "linux_parser.h"
 #include "ncurses_display.h"
 #include "system.h"
 
 int main() {
   System system;
-  // std::vector<std::string> out = LinuxParser::CpuUtilization();
-  // std::cout << LinuxParser::UpTime(1) << std::endl;
   NCursesDisplay::Display(system);
 }
